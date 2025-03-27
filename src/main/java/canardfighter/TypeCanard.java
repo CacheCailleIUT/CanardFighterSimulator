265bfc8cbfa056937080eeab46849159766cff62
Gabriel Monczewski <gabriel.monczewski@outlook.fr>
2025-03-27 18:39:14 +0100

    mutliplicateur done

--- src/main/java/canardfighter/TypeCanard.java
+++ src/main/java/canardfighter/TypeCanard.java
@@ -2,37 +2,30 @@ package canardfighter;
 
 public enum TypeCanard {
 
-    EAU, // 0
-    FEU, // 1
-    GLACE, // 2
-    VENT; // 3
+    EAU(0), // 0
+    FEU(1), // 1
+    GLACE(2), // 2
+    VENT(3); // 3
 
-    private final double[][] matriceEff =
+    public final int value;
+
+    TypeCanard(int value) {
+        this.value = value;
+    }
+
+    private final static double[][] matriceEff =
             new double[][] {
-                    {0, 0, 1.0},
-                    {0, 1, 1.5},
-                    {0, 2, 0.5},
-                    {0, 3, 0.5},
+                    {1.0, 1.5, 0.5, 0.5}, // EAU
 
-                    {1, 0, 0.5},
-                    {1, 1, 1.0},
-                    {1, 2, 1.5},
-                    {1, 3, 1.0},
+                    {0.5, 1.0, 1.5, 1.0}, // FEU
 
-                    {2, 0, 1.0},
-                    {2, 1, 0.5},
-                    {2, 2, 1.0},
-                    {2, 3, 1.5},
+                    {1.0, 0.5, 1.0, 1.5}, // GLACE
 
-                    {3, 0, 1.5},
-                    {3, 1, 1.0},
-                    {3, 2, 0.5},
-                    {3, 3, 1.0},
-
-
+                    {1.5, 1.0, 0.5, 1.0} // VENT
             };
+
     // TODO Multiplicateur d'attaque
     public static double getMultiplicateur(TypeCanard attaquant, TypeCanard cible) {
-        return 0.0; // Stub
+        return matriceEff[attaquant.value][cible.value];
     }
 }
